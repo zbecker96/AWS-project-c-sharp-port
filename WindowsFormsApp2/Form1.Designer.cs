77bc76f23250ab7793f1bfd55231749e684d037e
ZoeBecker <beckerc@usf.edu>
2019-04-02 18:34:25 -0700

    Add project files.

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace WindowsFormsApp2 {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.FilePathBox = new System.Windows.Forms.TextBox();
+            this.ChooseFileButton = new System.Windows.Forms.Button();
+            this.FilePathBoxLabel = new System.Windows.Forms.Label();
+            this.LabelButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.ResultsLabel = new System.Windows.Forms.Label();
+            this.ActionsLabel = new System.Windows.Forms.Label();
+            this.ResultsTextBox = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // FilePathBox
+            // 
+            this.FilePathBox.Location = new System.Drawing.Point(12, 420);
+            this.FilePathBox.Name = "FilePathBox";
+            this.FilePathBox.Size = new System.Drawing.Size(257, 20);
+            this.FilePathBox.TabIndex = 0;
+            // 
+            // ChooseFileButton
+            // 
+            this.ChooseFileButton.Location = new System.Drawing.Point(275, 417);
+            this.ChooseFileButton.Name = "ChooseFileButton";
+            this.ChooseFileButton.Size = new System.Drawing.Size(75, 23);
+            this.ChooseFileButton.TabIndex = 1;
+            this.ChooseFileButton.Text = "Choose File";
+            this.ChooseFileButton.UseVisualStyleBackColor = true;
+            this.ChooseFileButton.Click += new System.EventHandler(this.ChooseFileButton_Click);
+            // 
+            // FilePathBoxLabel
+            // 
+            this.FilePathBoxLabel.AutoSize = true;
+            this.FilePathBoxLabel.Location = new System.Drawing.Point(13, 398);
+            this.FilePathBoxLabel.Name = "FilePathBoxLabel";
+            this.FilePathBoxLabel.Size = new System.Drawing.Size(51, 13);
+            this.FilePathBoxLabel.TabIndex = 2;
+            this.FilePathBoxLabel.Text = "File Path:";
+            // 
+            // LabelButton
+            // 
+            this.LabelButton.Location = new System.Drawing.Point(12, 330);
+            this.LabelButton.Name = "LabelButton";
+            this.LabelButton.Size = new System.Drawing.Size(75, 23);
+            this.LabelButton.TabIndex = 3;
+            this.LabelButton.Text = "Labels";
+            this.LabelButton.UseMnemonic = false;
+            this.LabelButton.UseVisualStyleBackColor = false;
+            this.LabelButton.Visible = false;
+            this.LabelButton.Click += new System.EventHandler(this.LabelButton_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(16, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(772, 290);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // ResultsLabel
+            // 
+            this.ResultsLabel.AutoSize = true;
+            this.ResultsLabel.Location = new System.Drawing.Point(353, 308);
+            this.ResultsLabel.Name = "ResultsLabel";
+            this.ResultsLabel.Size = new System.Drawing.Size(42, 13);
+            this.ResultsLabel.TabIndex = 6;
+            this.ResultsLabel.Text = "Results";
+            // 
+            // ActionsLabel
+            // 
+            this.ActionsLabel.AutoSize = true;
+            this.ActionsLabel.Location = new System.Drawing.Point(12, 308);
+            this.ActionsLabel.Name = "ActionsLabel";
+            this.ActionsLabel.Size = new System.Drawing.Size(45, 13);
+            this.ActionsLabel.TabIndex = 7;
+            this.ActionsLabel.Text = "Actions:";
+            // 
+            // ResultsTextBox
+            // 
+            this.ResultsTextBox.Location = new System.Drawing.Point(356, 330);
+            this.ResultsTextBox.Name = "ResultsTextBox";
+            this.ResultsTextBox.Size = new System.Drawing.Size(432, 110);
+            this.ResultsTextBox.TabIndex = 8;
+            this.ResultsTextBox.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ResultsTextBox);
+            this.Controls.Add(this.ActionsLabel);
+            this.Controls.Add(this.ResultsLabel);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.LabelButton);
+            this.Controls.Add(this.FilePathBoxLabel);
+            this.Controls.Add(this.ChooseFileButton);
+            this.Controls.Add(this.FilePathBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox FilePathBox;
+        private System.Windows.Forms.Button ChooseFileButton;
+        private System.Windows.Forms.Label FilePathBoxLabel;
+        private System.Windows.Forms.Button LabelButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label ResultsLabel;
+        private System.Windows.Forms.Label ActionsLabel;
+        private System.Windows.Forms.RichTextBox ResultsTextBox;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Amazon.Rekognition;
+using Amazon.Rekognition.Model;
+using System.Windows.Forms;
+
+
+namespace WindowsFormsApp2 {
+
+
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        public String[] getCredentials() {
+            String[] credentials = { "", "" };
+
+            //set up astream reader
+            StreamReader csvReader = new StreamReader("C:\\Users\\Zoe Becker\\Desktop\\accessKeys.csv");
+            String data = csvReader.ReadLine();
+            String[] splitData;
+            
+            data = csvReader.ReadLine();
+
+            splitData = data.Split(',');
+
+            credentials[0] = splitData[0];//0th pos is accesskey 1th secretkey
+            credentials[1] = splitData[1];//0th pos is accesskey 1th secretkey
+
+            return credentials;
+        }
+       
+        public void processFile(Amazon.Rekognition.Model.Image imageData) {
+
+            String[] loginCreds = getCredentials();
+
+            var credentials = new Amazon.Runtime.BasicAWSCredentials(loginCreds[0],loginCreds[1]);
+
+            AmazonRekognitionClient rekognitionClient = new AmazonRekognitionClient(credentials, Amazon.RegionEndpoint.USEast2);
+
+            DetectLabelsRequest detectlabelsRequest = new DetectLabelsRequest() {
+                Image = imageData,
+                MaxLabels = 10,
+                MinConfidence = 77F
+            };
+
+            try {
+                DetectLabelsResponse detectLabelsResponse = rekognitionClient.DetectLabels(detectlabelsRequest);
+
+                //string builder to store labels
+                StringBuilder labels = new StringBuilder();
+
+                bool isHotdog = false;
+                foreach (Amazon.Rekognition.Model.Label label in detectLabelsResponse.Labels) {
+                    if (label.Name == "Hot Dog") { isHotdog = true; }
+
+                    //adds all the labels to a string
+                    labels.AppendFormat("{0}: {1} \n", label.Name, label.Confidence);
+
+                }
+                if (isHotdog) {
+                    labels.Append("\n is a hotdog\n");
+                } else {
+                    labels.Append("\n is not a hotdog\n");
+                }
+
+                ResultsTextBox.Text = labels.ToString();
+
+
+            } catch (Exception e) {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+
+
+        Amazon.Rekognition.Model.Image imgData = new Amazon.Rekognition.Model.Image();
+
+        private void ChooseFileButton_Click(object sender, EventArgs e) {
+
+            //upon click load a new file
+            OpenFileDialog newFile = new OpenFileDialog();
+
+            if (newFile.ShowDialog() == DialogResult.OK) {
+                //get the image bit map
+                Bitmap imgBits = new Bitmap(newFile.FileName);
+                pictureBox1.Image = imgBits;//set the picture box to the image
+                pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
+                FilePathBox.Text = newFile.FileName;
+
+                try {
+                    using (FileStream fs = new FileStream(newFile.FileName, FileMode.Open, FileAccess.Read)) {
+                        byte[] data = null;
+                        data = new byte[fs.Length];
+                        fs.Read(data, 0, (int)fs.Length);
+                        imgData.Bytes = new MemoryStream(data);
+                    }
+                } catch (Exception) {
+                    Console.WriteLine("Failed to load file " + newFile.FileName);
+                    return;
+                }
+            }
+
+            this.LabelButton.Visible = true;
+            return;
+
+        }
+
+        private void LabelButton_Click(object sender, EventArgs e) {
+
+            processFile(imgData);
+        }
+    }
+}
